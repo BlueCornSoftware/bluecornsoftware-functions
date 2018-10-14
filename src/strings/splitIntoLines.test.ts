5e6fee0664048550b8e900806835e0d1dac9ee63
JakeTheCorn <JakeTheCorn@gmail.com>
2018-10-14 06:21:53 -0400

    still spec-ing out splitIntoLines.test.ts

--- src/strings/splitIntoLines.test.ts
+++ src/strings/splitIntoLines.test.ts
@@ -1,17 +1,19 @@
 import { linesOfMaxLength } from './linesOfMaxLength'
 
-const splitIntoLines = (sentence: string, maxLength: number): string[] => {
-  let lines = linesOfMaxLength(sentence, maxLength)
-  // Remove all leading and trailing whitespace
-  lines = lines.map(line => line.trim())
 
-  // shuffle back where each line has fewer than max elements
+const splitIntoLines = (sentence: string, maxLength: number): string[] => {
+  const lines = linesOfMaxLength(sentence, maxLength)
+
   return lines
 }
 
 describe('splitIntoLines', () => {
   it('should not break spaces across lines', () => {
     const lines = splitIntoLines('Hey You Guys', 3)
-    expect(lines[1][0]).not.toBe(' ')
+    expect(lines[1]).toBe('You')
+  })
+  it('breaks words that are longer than line length', () => {
+    const lines = splitIntoLines('OnceUponATime you dressed so fine', 4)
+    expect(lines[1]).toBe('Upon')
   })
 })
