28dfc6b7782a1c6ceea67923043e15acbb706a35
JakeTheCorn <JakeTheCorn@gmail.com>
2018-10-27 12:35:23 -0400

    reformat flatmap

--- src/array/flatMap.ts
+++ src/array/flatMap.ts
@@ -1,3 +1,4 @@
 const concat = (x: any[], y: any[]) => x.concat(y)
 
-export const flatMap = (array: any[], func: (value: any, index?: number, array?: any[]) => any[]) => array.map(func).reduce(concat, [])
+export const flatMap = (array: any[], func: (value: any, index?: number, array?: any[]) => any[]): any[] =>
+  array.map(func).reduce(concat, [])
