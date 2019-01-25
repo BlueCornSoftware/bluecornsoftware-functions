a287d8c0b57f701daf970ce04aca29c8727b8ffd
jakethecorn <jakethecorn@gmail.com>
2019-01-24 21:29:33 -0500

    use generic typy in omit func

--- src/object/omit.ts
+++ src/object/omit.ts
@@ -1,5 +1,5 @@
-export function omit(obj, ...bannedKeys) {
-  const copy = { ...obj }
+export function omit<T extends object>(obj: T, ...bannedKeys): Partial<T> {
+  const copy: T = { ...obj as object } as T
   for (let i = 0; i < bannedKeys.length; i++) {
     delete copy[bannedKeys[i]]
   }
